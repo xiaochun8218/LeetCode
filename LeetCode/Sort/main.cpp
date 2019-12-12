d8fa9e8995f129482c99ae50075410a509792af8
paulxiao <525231866@qq.com>
2019-12-12 23:44:02 +0800

    Sort/main.cpp

--- LeetCode/Sort/main.cpp
+++ LeetCode/Sort/main.cpp
@@ -17,18 +17,68 @@ void insertSort(int arr[], int len)
     }
 }
 
+//—°‘Ò≈≈–Ú
+void selectSort(int arr[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		int min = i;
+		for (int j = i + 1; j < len; j++)
+		{
+			if (arr[j] < arr[min])
+			{
+				min = j;
+			}
+		}
+		int temp = arr[min];
+		arr[min] = arr[i];
+		arr[i] = temp;
+	}
+}
+
 //√∞≈›≈≈–Ú
 void bubbleSort(int arr[], int len)
 {
-    for (int i = 0; i < len; i++)
-    {
-        for (int j = 0; j < len - i; j++)
-        {
-            int temp = arr[j];
-            arr[j] = arr[j + 1];
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = 0; j < len - i; j++)
+		{
+			int temp = arr[j];
+			arr[j] = arr[j + 1];
 			arr[j + 1] = temp;
-        }
-    }
+		}
+	}
+}
+
+//øÏÀŸ≈≈–Ú
+int partition(int arr[], int left, int right)
+{
+	int key = arr[left];
+
+	int i = left;
+	int j = right;
+	while (i != j)
+	{
+		while ((i < j) && (arr[j] >= key))
+			j--;
+		arr[i] = arr[j];
+		while ((i < j) && (arr[i] <= key))
+			i++;
+		arr[j] = arr[i];
+	}
+	arr[i] = key;
+
+	return i;
+}
+
+void quickSort(int arr[], int left, int right)
+{
+	if (left < right)
+	{
+		int i = partition(arr, left, right);
+		quickSort(arr, left, i - 1);
+		quickSort(arr, i + 1, right);
+	}
 }
 
 int main()
