7ff438b18bc19326dacf233318ca229c4e535381
paulxiao(肖纯) <chun.xiao@upchina.com>
2020-01-13 11:24:09 +0800

    BinarySearch

--- LeetCode/RecursionAndBacktracking/main.cpp
+++ LeetCode/RecursionAndBacktracking/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+//汉诺塔
+void move(int n, char x, char z)
+{
+    cout << "num:" << n  <<  "x-->z" << endl;
+}
+
+void hannoi(int n, char x, char y, char z)
+{
+    if (n == 1)
+    {
+        return;
+    }
+
+    hannoi(n - 1, x, z, y);
+    move(n, x, z);
+    hannoi(n - 1, y, x, z);
+}
+
 //N皇后
 int nCount = 0;
 bool check(int row, int col, int solution[])
@@ -41,6 +59,40 @@ int totalCount(int n)
 	nCount = 0;
 	backtracking(n, 0, new int[n]);
 	return nCount;
+}
+
+
+//二分查找
+int binarySearch(int arr[], const int key, int low, int high)
+{
+    if (low > high)
+    {
+        return -1;
+    }
+
+    //用 mid = (low + high) / 2 会有溢出风险
+    /*
+      low  = 0100 0000 0000 0000 0000 0000 0000 0000 
+      high = 0100 0000 0000 0000 0000 0000 0000 0000 
+
+      low + high = 1000 0000 0000 0000 0000 0000 0000 0000 
+      int 有符号位，符号位为1表示负数，负数在机器中以补码形式保存
+      数值位 取反加一得：1000 0000 0000 0000 0000 0000 0000 0000 = 2147483648
+      low + high = -2147483648
+    */
+    int mid = low + (high - low) / 2;
+    if (arr[mid] == key)
+    {
+        return mid;
+    }
+    else if (arr[mid] < key)
+    {
+        return binarySearch(arr, key, low, mid - 1);
+    }
+    else
+    {
+        return binarySearch(arr, key, mid + 1, high);
+    }
 }
 
 int main()
